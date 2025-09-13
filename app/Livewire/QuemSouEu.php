2c973de66ace6fd348b354bc7e74464d18a408de
bernardo-lucas <lucas.bernardo.silv@gmail.com>
2025-09-13 15:07:48 -0300

    Cria componentes Livewire para cada seção do portfólio

---
+++ app/Livewire/QuemSouEu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class QuemSouEu extends Component
+{
+    public function render()
+    {
+        return view('livewire.quem-sou-eu');
+    }
+}
