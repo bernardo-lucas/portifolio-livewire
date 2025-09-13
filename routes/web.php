6e3a1307d28f1021be4e0e31fd8ac52e1a561cdb
bernardo-lucas <lucas.bernardo.silv@gmail.com>
2025-09-13 15:12:16 -0300

    Define rotas para os componentes do portfólio

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Home;
+use App\Livewire\QuemSouEu;
+use App\Livewire\Sobre;
+use App\Livewire\Projetos;
+use App\Livewire\Contato;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', Home::class);
+Route::get('/quem-sou-eu', QuemSouEu::class);
+Route::get('/sobre', Sobre::class);
+Route::get('/projetos', Projetos::class);
+Route::get('/contato', Contato::class);
